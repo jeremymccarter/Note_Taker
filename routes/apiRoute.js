08b3d989cfb0fb539ecc5a539c3be693e3b32d73
Jeremy McCarter <jeremymccarter27@gmail.com>
2023-03-02 16:50:33 -0800

    added note saving functionality

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const fs = require('fs')
 const router = require('express').Router();
+var uniqid = require('uniqid');
 
 
 router.get('/notes', (req, res) => {
@@ -16,19 +17,27 @@ router.post('/notes', (req, res) => {
 const { title,text } = req.body
 const newNote = {
 title,
-text
-
+text,
+id: uniqid()
 }
 fs.readFile('db/db.json', 'utf8', (err, data) => {
 if(err){
     console.log(err)
 }
-const parseNotes = json.parse(data)
+const parseNotes = JSON.parse(data)
 console.log(parseNotes)
 parseNotes.push(newNote)
-fs.writeFile('')
-})
+fs.writeFile('db/db.json', JSON.stringify(parseNotes), (err) => {
+    if(err){
+        console.log(err)
 
+    }
+    console.log('Note Saved!')
+
+});
+
+res.json(parseNotes)
+})
 })
 
 
